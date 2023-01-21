c9036457f4ffe88135429184c5409634ffa75578
Luiz Gabriel <lgabrielpessoal@gmail.com>
2023-01-21 18:35:40 -0300

    fix: Variaveis do servidor

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -3,10 +3,10 @@ import { Options } from 'sequelize';
 
 const config: Options = {
   username: process.env.DB_USER || 'postgres',
-  password: process.env.DB_PASS || '123456',
-  database: 'NEW_GENERATION',
-  host: process.env.DB_HOST || 'localhost',
-  port: Number(process.env.DB_PORT) || 5432,
+  password: process.env.DB_PASS || '7hjRGRCtY5aV3ZrAyxFl',
+  database: 'railway',
+  host: process.env.DB_HOST || 'containers-us-west-140.railway.app',
+  port: Number(process.env.DB_PORT) || 6488,
   dialect: 'postgres'
 }
 
